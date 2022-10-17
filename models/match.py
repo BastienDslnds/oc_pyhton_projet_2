990197f938fc9e6a0c4e306a9dfe5df8dfcb516f
Bastien Deslandes <bastien.deslandes@free.fr>
2022-10-17 09:55:39 +0200

    stockage d'un match sous forme de tuple

--- models/match.py
+++ models/match.py
@@ -17,10 +17,26 @@ class Match:
     """Match"""
 
     def __init__(self, score_un='', score_deux=''):
-        """Initialise un match"""
+        """Initialize a match"""
+
         self.pair: List[Player] = PairPlayers()
         self.score_un = score_un
         self.score_deux = score_deux
 
+    def store_match(self):
+        """Store a match in the form of a tuble containing two lists.
+        Ecah list contains a player and a score."""
+
+        match = ([self.pair[0], self.score_un], [self.pair[1], self.score_deux])
+        return match
+
     def __str__(self):
         return f"([{self.pair[0]}, {self.score_un}], [{self.pair[1]}, {self.score_deux}])"
+
+    def serialized_match(self):
+        serialized_match = {
+            'pair': self.pair,
+            'score_un': self.score_un,
+            'score_deux': self.score_deux,
+        }
+        return serialized_match
