15ba104f07171c64fd698e1e42887eeea1448ee3
Bastien Deslandes <bastien.deslandes@free.fr>
2022-10-19 17:47:20 +0200

    maj avec un fonctionnement par menu

--- main.py
+++ main.py
@@ -1,46 +1,43 @@
 """Entry point."""
 
 from controllers.base import Controller
-
+from views.manager import ManagerView
 from models.tournament import Tournament
 from models.player import Player
 
-from views.player import PlayerView
-
 
 def main():
-    """game = Controller()
-    game.run()"""
+    """game.run"""
 
 
 if __name__ == "__main__":
     """main()"""
-    active_view = PlayerView()
 
-    game = Controller(active_view)
+    manager_view = ManagerView()  # creation de la vue pour la saisie d'informations par le manager
+    game = Controller(manager_view)  # creation du controller permettant de contrôler les étapes d'un tournoi
 
-    tournament = Tournament("Tournoi initial", "Paris", "22/09/2022")
+    game.tournament = Tournament("1", "Tournoi initial", "Paris", "22/09/2022")
 
     joueur_un = Player("Bastien", "Deslandes", "10/11/1995", "M", 1)
-    tournament.players.append(joueur_un)
+    game.tournament.players.append(joueur_un)
     joueur_deux = Player("Sam", "Idilbi", "15/06/1995", "M", 4)
-    tournament.players.append(joueur_deux)
+    game.tournament.players.append(joueur_deux)
     joueur_trois = Player("Pierre", "Leparoux", "10/11/1995", "M", 5)
-    tournament.players.append(joueur_trois)
+    game.tournament.players.append(joueur_trois)
     joueur_quatre = Player("Hugo", "Crosnier", "10/11/1995", "M", 2)
-    tournament.players.append(joueur_quatre)
+    game.tournament.players.append(joueur_quatre)
     joueur_cinq = Player("Camille", "Guillaume", "10/11/1995", "M", 3)
-    tournament.players.append(joueur_cinq)
+    game.tournament.players.append(joueur_cinq)
     joueur_six = Player("Ambroise", "Leporcher", "10/11/1995", "M", 8)
-    tournament.players.append(joueur_six)
+    game.tournament.players.append(joueur_six)
     joueur_sept = Player("Hugo", "Perrin", "10/11/1995", "M", 6)
-    tournament.players.append(joueur_sept)
+    game.tournament.players.append(joueur_sept)
     joueur_huit = Player("Jean", "Coquet", "10/11/1995", "M", 7)
-    tournament.players.append(joueur_huit)
+    game.tournament.players.append(joueur_huit)
 
-    for tour in range(int(tournament.nb_rounds)):  # boucle pour gérer les 4 tours du tournoi
-        game.open_round(tournament)
-        game.get_matchs_pairs(tournament, tournament.rounds[tour])  # Créer les paires de chaque match du tour
-        game.get_matchs_results(tournament.rounds[tour])  # Récupérer les résultats de chaque match
-        game.close_round(tournament.rounds[tour])  # Fermer le tour
+    game.tournament.save_players()
+    game.tournament.save_tournament()
+
+    game.run()  # lancement d'un tournoi"""
+
 
